2fe362c9b5141cca33f02b334f1a2c46a442dfdf
Sari Setianingsih <sari.thok@gmail.com>
2019-02-20 00:40:09 +0100

    Add parsing for masjid table

---
+++ wde/core/parse/masjid/listing.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from collections import namedtuple
+
+from bs4 import BeautifulSoup
+
+
+MASJID_LISTING_HOME_URL = 'http://simas.kemenag.go.id/index.php/profil/masjid/page/'
+MASJID_LISTING_URL = 'http://simas.kemenag.go.id/index.php/profil/masjid/page/%d'   # page id (multiple of 10)
+
+SDM = namedtuple('SDM', ['jamaah', 'imam', 'khatib', 'muazin', 'remaja'])
+
+
+class ListingParser:
+
+    @classmethod
+    def get_last_page_id(cls, content):
+        soup = BeautifulSoup(content, 'html.parser')
+        last_url = soup.find('div', class_='paging').findAll('a', href=True)[-1]['href']
+        return cls._parse_paging_id_from_url(last_url)
+
+    @classmethod
+    def extract(cls, content):
+        soup = BeautifulSoup(content, 'html.parser').find('div', class_='wrap')
+        main_table = soup.find('table', class_='widefat border').find('tbody')
+        results = {}
+        for tr in main_table.find_all('tr'):
+            tds = tr.find_all('td')
+            if tds:
+                masjid_url = tds[3].find('a', href=True)['href']
+                masjid_id = cls._parse_masjid_id_from_url(masjid_url)
+                jamaah = tds[11].text
+                imam = tds[12].text
+                khatib = tds[13].text
+                muazin = tds[14].text
+                remaja = tds[15].text
+                results[masjid_id] = SDM(jamaah, imam, khatib, muazin, remaja)
+        return results
+
+    @classmethod
+    def _parse_paging_id_from_url(cls, url):
+        # Example: http://simas.kemenag.go.id/index.php/profil/masjid/page/250250
+        return url.split('/')[-1]
+
+    @classmethod
+    def _parse_masjid_id_from_url(cls, url):
+        # Example: http://simas.kemenag.go.id/index.php/profil/masjid/276353/
+        return url.split('/')[-2]
---
+++ wde/core/parse/masjid/test/listing.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
+from wde.core.parse.masjid.listing import (
+    ListingParser,
+    MASJID_LISTING_HOME_URL,
+    SDM)
+from wde.core.utils.io import (
+    get_content,
+    read_file,
+)
+
+
+def offline():
+    return read_file('wde/core/parse/masjid/test/html/masjid_listing.html')
+
+
+def online():
+    return get_content(MASJID_LISTING_HOME_URL)
+
+
+@pytest.mark.parametrize('source, expected', [
+    (offline,     '250320'),
+    (online,      '250320'),
+])
+def test_listing_get_last_page_id(source, expected):
+    last_page_id = ListingParser.get_last_page_id(source())
+    assert last_page_id == expected
+
+
+@pytest.mark.parametrize('source', [
+    offline,
+    online,
+])
+def test_listing_extract(source):
+    result = ListingParser.extract(source())
+    assert len(result) == 10
+    assert all([isinstance(sdm, SDM) for sdm in result.values()])
+    assert result['276472'] == SDM(jamaah='> 200', imam='1', khatib='1', muazin='1', remaja='27')
